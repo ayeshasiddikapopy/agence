03282c6d311245a18a9d35bc30c22306c6a1f786
ayesha siddika popy <99285521+ayeshasiddikapopy@users.noreply.github.com>
2022-09-15 04:24:22 +0600

    code added from nav to choose section

---
+++ js/tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary:'#6A4DF4',
+          primaryShade:'#F8F6FE',
+          overlay:'#F3F3F3',
+          gold:'#F9BD2C',
+          black:' #151515',
+          gray:'#737373',
+        },
+        maxWidth: {
+            container:'1320px',
+          },
+          fontFamily: {
+            inter:"'Inter', 'sans-serif'",
+          }  
+      }
+    }
+  }
